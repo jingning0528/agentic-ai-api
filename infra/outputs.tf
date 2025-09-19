cf1402b8ef33b526e31108578bec93aa7155d35d
Ning <Jing.Ning@gov.bc.ca>
2025-09-19 10:18:58 -0700

    Azure Deploy

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,1 +1,16 @@
+output "RESOURCE_GROUP_ID" {
 
+  value = azurerm_resource_group.main.id
+}
+
+output "AZURE_CONTAINER_REGISTRY_ENDPOINT" {
+  value = azurerm_container_registry.main.login_server
+}
+
+output "CONTAINER_APP_FQDN" {
+  value = azurerm_container_app.api.latest_revision_fqdn
+}
+
+output "CONTAINER_APP_NAME" {
+  value = azurerm_container_app.api.name
+}
