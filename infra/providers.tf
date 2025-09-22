f9e8d980ea24ac9dec31482a5cdb9f7b75761e9f
Ning <Jing.Ning@gov.bc.ca>
2025-09-22 15:16:51 -0700

    Terraform

--- infra/providers.tf
+++ infra/providers.tf
@@ -26,8 +26,4 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = false
     }
   }
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  use_oidc        = var.use_oidc
-  client_id       = var.client_id
 }
